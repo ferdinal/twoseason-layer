e54f37e0bcdc223895392b100f1525b6360108cb
ferdinal <ferdinal13@gmail.com>
2022-02-04 13:36:06 +0700

    create mongo connection

--- internalLibraries/nodejs/helpers/dbConnection.js
+++ internalLibraries/nodejs/helpers/dbConnection.js
@@ -2,61 +2,61 @@ let mongoose = require("mongoose");
 mongoose.Promise = require("bluebird");
 
 exports.dbConnection = () => {
-  const Schema = mongoose.Schema;
-  const ObjectId = Schema.ObjectId;
+	const Schema = mongoose.Schema;
+	const ObjectId = Schema.ObjectId;
 
-  const conn = mongoose
-    .connect(
-      "mongodb+srv://dailybox:Dailybox12345!@dailybox-m3uld.mongodb.net/test?retryWrites=true&w=majority",
-      {
-        useUnifiedTopology: true,
-        dbName: "sgi",
-        useFindAndModify: false,
-        useNewUrlParser: true,
-        useCreateIndex: true,
-      }
-    )
-    .then(console.log("connecting success"))
-    .catch(console.log("conneting error"));
+	const conn = mongoose
+		.connect(
+			"mongodb+srv://Z1HbRfsRSlirQcTb:Z1HbRfsRSlirQcTb@cluster0.tkpuw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+			{
+				useUnifiedTopology: true,
+				dbName: "sgi",
+				useFindAndModify: false,
+				useNewUrlParser: true,
+				useCreateIndex: true,
+			}
+		)
+		.then(console.log("connecting success"))
+		.catch(console.log("conneting error"));
 
-  mongoose.connection.once("open", function () {
-    console.log("Mongoose connection in lambda opened");
-  });
+	mongoose.connection.once("open", function () {
+		console.log("Mongoose connection in lambda opened");
+	});
 
-  mongoose.connection.on("error", function () {
-    console.error("Error creating mongoose connection in lambda, exiting!");
-    process.exit(1);
-  });
+	mongoose.connection.on("error", function () {
+		console.error("Error creating mongoose connection in lambda, exiting!");
+		process.exit(1);
+	});
 
-  return conn;
+	return conn;
 };
 
 exports.dbConnectionDev = () => {
-  const Schema = mongoose.Schema;
-  const ObjectId = Schema.ObjectId;
+	const Schema = mongoose.Schema;
+	const ObjectId = Schema.ObjectId;
 
-  const conn = mongoose
-    .connect(
-      "mongodb+srv://dailybox:Dailybox12345!@dailybox-m3uld.mongodb.net/test?retryWrites=true&w=majority",
-      {
-        useUnifiedTopology: true,
-        dbName: "development",
-        useFindAndModify: false,
-        useNewUrlParser: true,
-        useCreateIndex: true,
-      }
-    )
-    .then(console.log("connecting success"))
-    .catch(console.log("conneting error"));
+	const conn = mongoose
+		.connect(
+			"mongodb+srv://Z1HbRfsRSlirQcTb:Z1HbRfsRSlirQcTb@cluster0.tkpuw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+			{
+				useUnifiedTopology: true,
+				dbName: "development",
+				useFindAndModify: false,
+				useNewUrlParser: true,
+				useCreateIndex: true,
+			}
+		)
+		.then(console.log("connecting success"))
+		.catch(console.log("conneting error"));
 
-  mongoose.connection.once("open", function () {
-    console.log("Mongoose connection in lambda opened");
-  });
+	mongoose.connection.once("open", function () {
+		console.log("Mongoose connection in lambda opened");
+	});
 
-  mongoose.connection.on("error", function () {
-    console.error("Error creating mongoose connection in lambda, exiting!");
-    process.exit(1);
-  });
+	mongoose.connection.on("error", function () {
+		console.error("Error creating mongoose connection in lambda, exiting!");
+		process.exit(1);
+	});
 
-  return conn;
+	return conn;
 };
